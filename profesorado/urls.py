fb9adbe7be8871f0d60a026850d97620fe56a6d1
Javier Garcia Dominguez <gardojavi@gmail.com>
2023-11-30 00:48:50 +0000

    Redirect en el logo a pagina de inicio

--- profesorado/urls.py
+++ profesorado/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = 'profesorado'
+
 urlpatterns = [
-    # post views
     path('', views.profesorado_list, name='profesorado_list'),
     path('<slug:profe_slug>/', views.profesorado_detail, name='profesorado_detail'),
 ]
